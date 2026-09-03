ac6a68c80f7d7c082db8dd396577aaff205f4f12
DrCas <133929112+DrCas@users.noreply.github.com>
2026-09-03 10:28:36 -0700

    Make Piglin Brute AI respect neutral engagement

--- src/fabricMain/java/io/github/drcas/gentlemobs/fabric/mixin/PiglinBruteAiMixin.java
+++ src/fabricMain/java/io/github/drcas/gentlemobs/fabric/mixin/PiglinBruteAiMixin.java
@@ -1,7 +1,6 @@
 package io.github.drcas.gentlemobs.fabric.mixin;
 
 import io.github.drcas.gentlemobs.fabric.GentleMobsFabric;
-import io.github.drcas.gentlemobs.fabric.GentleMode;
 import java.util.Optional;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
@@ -22,12 +21,9 @@ public abstract class PiglinBruteAiMixin {
             AbstractPiglin body,
             CallbackInfoReturnable<Optional<? extends LivingEntity>> cir
     ) {
-        if (GentleMobsFabric.getGlobalMode() != GentleMode.PASSIVE) {
-            return;
-        }
-
         Optional<? extends LivingEntity> target = cir.getReturnValue();
-        if (target != null && target.isPresent() && target.get() instanceof Player) {
+        if (target != null && target.orElse(null) instanceof Player player
+                && !GentleMobsFabric.canTargetPlayer(body, player)) {
             cir.setReturnValue(Optional.empty());
         }
     }
